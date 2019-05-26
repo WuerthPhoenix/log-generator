418dd9abdfd5d46926aa70eb9c05140897815ea3
Fedele Mantuano <mantuano.fedele@gmail.com>
2019-05-26 22:10:53 +0200

    Added new Python version in Travis

--- setup.py
+++ setup.py
@@ -34,8 +34,6 @@ setup(
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
